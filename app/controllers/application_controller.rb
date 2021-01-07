4586e8c7864120782cf05b38d01ebce9c5a53edb
Jake Heft <jakeheft@gmail.com>
2021-01-06 19:42:02 -0700

    Add get for electric utilities
    Co-authored-by: Christopher Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Hanna Davis <63931068+Oxalisviolacea@users.noreply.github.com>
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,16 @@ class ApplicationController < Sinatra::Base
     JSON.generate(message: "Welcome to the Sinatra Template!")
   end
 
+  get '/:location/utilities/electricity' do
+    conn = Faraday.new('https://api.yelp.com') do |f|
+      f.headers['Authorization'] = ENV['YELP_API_KEY']
+    end
+    response = conn.get("/v3/businesses/search?categories=electricitysuppliers&location=#{params[:location]}")
+    json = JSON.parse(response.body, symbolize_names: true)
+    render json: UtilitySerializer.new(json)
+    # require 'pry'; binding.pry
+  end
+
   # get '/:name' do
   #   empty_string = ""
   #   params.keys.each do |k|
