2e5240de23c1285e4c306b8e5fcca03388321645
Dani Coleman <danicoleman00@gmail.com>
2021-01-06 17:21:37 -0700

    Comment out practice code implemented with Sal for GET requests
    Co-authored-by: Christopher Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Hanna Davis <63931068+Oxalisviolacea@users.noreply.github.com>
    Co-authored-by: Jake Heft <60531761+jakeheft@users.noreply.github.com>
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < Sinatra::Base
   get '/' do
     JSON.generate(message: "Welcome to the Sinatra Template!")
   end
+
+  # get '/:name' do
+  #   empty_string = ""
+  #   params.keys.each do |k|
+  #     empty_string += params[k]
+  #   end
+  #   JSON.generate(message: "Welcome #{params[:name]}. I like #{empty_string} too!")
+  # end
 end
