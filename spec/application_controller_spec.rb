b425b3c779d116d25ae3b74fbc7b2a4b6f11c378
Jake Heft <jakeheft@gmail.com>
2021-01-06 19:43:20 -0700

    Add get for electric utilities
    Co-authored-by: Christopher Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: Dani Coleman <60626984+dcoleman21@users.noreply.github.com>
    Co-authored-by: Hanna Davis <63931068+Oxalisviolacea@users.noreply.github.com>
    Co-authored-by: Jose Lopez <61892154+JoseLopez235@users.noreply.github.com>
    Co-authored-by: Kevin Cuadros <63522369+kevxo@users.noreply.github.com>

--- spec/application_controller_spec.rb
+++ spec/application_controller_spec.rb
@@ -13,4 +13,24 @@ describe ApplicationController do
 
     expect(body[:message]).to include("Welcome to the Sinatra Template!")
   end
+
+  it 'can get utility data' do
+    location = 80211
+    get "/#{location}/utilities/electricity"
+      expect(response).to be_successful
+      json = JSON.parse(response.body, symbolize_names: true)
+      items = json[:data]
+      items.each do |item|
+        expect(item).to have_key(:name)
+        expect(item[:name]).to be_an(String)
+        expect(item[:attributes]).to have_key(:is_closed)
+        expect(item[:attributes][:is_closed]).to be_a(Boolean)
+        expect(item[:attributes]).to have_key(:description)
+        expect(item[:attributes][:description]).to be_a(String)
+        expect(item[:attributes]).to have_key(:unit_price)
+        expect(item[:attributes][:unit_price]).to be_a(Float)
+        expect(item[:attributes]).to have_key(:merchant_id)
+        expect(item[:attributes][:merchant_id]).to be_a(Integer)
+      end
+  end
 end
