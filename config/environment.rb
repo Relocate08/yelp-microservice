7d3ee93572e18c5c873d6ba9c06ea2e714a581c4
Jake Heft <jakeheft@gmail.com>
2021-01-12 11:00:54 -0700

    Remove active support gem
    Co-authored-by: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>
    Co-authored-by: Christopher Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

--- config/environment.rb
+++ config/environment.rb
@@ -3,8 +3,8 @@ require "rubygems"
 require "bundler"
 Bundler.require(:default, ENV['SINATRA_ENV'])
 Bundler.require(Sinatra::Base.environment)
-require "active_support/deprecation"
-require "active_support/all"
+# require "active_support/deprecation"
+# require "active_support/all"
 $db = []
 
 require_rel '../app'
