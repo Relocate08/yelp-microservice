263c4f54b5b11107c2b39e763719f884c32190f1
Jake Heft <jakeheft@gmail.com>
2021-01-12 11:24:39 -0700

    Refactor response in fetch parsed json method
    Co-authored-by: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>
    Co-authored-by: Christopher Allbritton <63476564+Callbritton@users.noreply.github.com>
    Co-authored-by: John Kim <65986168+abcdefghijohn@users.noreply.github.com>

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -1,14 +1,15 @@
 require './config/environment'
-require 'sinatra'
 require 'json'
-require 'faraday'
 
 class YelpService < Sinatra::Base
 	def self.fetch_parsed_json(category, location)
 		conn = Faraday.new('https://api.yelp.com') do |f|
       f.headers['Authorization'] = ENV['YELP_API_KEY']
 		end
-		response = conn.get("/v3/businesses/search?categories=#{category}&location=#{location}")
+		response = conn.get("/v3/businesses/search") do |f|
+			f.params[:location] = location
+			f.params[:categories] = category
+		end
 		parsed_json = JSON.parse(response.body, symbolize_names: true)
 	end
 end
