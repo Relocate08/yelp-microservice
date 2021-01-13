687ebf6e5d3b50e03f6bbfb55c3a368922f81878
Jake Heft <jakeheft@gmail.com>
2021-01-12 19:40:08 -0700

    Add fetch movies method to yelp service
    Co-authored-by: Brett Sherman <64513280+BJSherman80@users.noreply.github.com>

--- app/services/yelp_service.rb
+++ app/services/yelp_service.rb
@@ -12,4 +12,20 @@ class YelpService < Sinatra::Base
 		end
 		parsed_json = JSON.parse(response.body, symbolize_names: true)
 	end
+
+	def self.fetch_events(location)
+		conn = Faraday.new('https://api.yelp.com') do |f|
+      f.headers['Authorization'] = ENV['YELP_API_KEY']
+		end
+		response = conn.get("/v3/events") do |f|
+			f.params[:location] = location
+			f.params[:limit] = 5
+			f.params[:start_date] = Time.now.to_i
+		end
+		parsed_json = JSON.parse(response.body, symbolize_names: true)
+	end
+
+	# def self.current_time
+	# 	Time.now.to_i
+	# end
 end
